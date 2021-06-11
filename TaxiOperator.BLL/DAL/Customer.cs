f2a51c3a8e9085f0e5571d6aa908442bdd85aa72
karamakil <k.akil@everteam-gs.com>
2021-06-11 13:38:18 +0300

    Enhancement in the documentations

--- TaxiOperator.BLL/DAL/Customer.cs
+++ TaxiOperator.BLL/DAL/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -11,7 +12,7 @@ namespace TaxiOperator.BLL.DAL
         {
             Addresses = new HashSet<Address>();
         }
-
+        [Required]
         public int Id { get; set; }
         public string Name { get; set; }
         public int? VipRation { get; set; }
